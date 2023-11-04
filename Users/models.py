d544bc70eb8e41d791023694c73f747ce60cd63b
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-11-04 16:09:01 +0100

    renamed the Users app (Singular)

--- Users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-class CustomUser(AbstractUser):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
