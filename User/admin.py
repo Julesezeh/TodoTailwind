16dcb6364b36969453a802097b89d98499bbdd2f
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-11-05 16:06:57 +0100

    done with initial configs

--- User/admin.py
+++ User/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .forms import CustomUserChangeForm, CustomUserRegistration
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+class CustomAdmin(UserAdmin):
+    add_form = CustomUserRegistration
+    model = CustomUser
+    form = CustomUserChangeForm
+    list_display = ["username","email"]
+
+
+admin.site.register(CustomUser,CustomAdmin)
