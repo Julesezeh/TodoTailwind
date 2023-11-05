f135356f1f951161dfafe0f64c7ba13a01483474
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-11-05 12:26:32 +0100

    Custom User creation and update forms

--- User/forms.py
+++ User/forms.py
@@ -5,3 +5,9 @@ class CustomUserRegistration(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ("first_name","username","email") 
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ("username","email","first_name")
