2eda447c83a6966e5c5e57fad54918a266ef675f
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-11-12 12:13:09 +0100

    Provided the intent of some fields

--- Todo/models.py
+++ Todo/models.py
@@ -8,8 +8,15 @@ class Todo(models.Model):
     user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
     description = models.TextField()
     time_created = models.DateTimeField(auto_now_add=True)
+
+    #Send notification to the User's email when the time starts to get close (30 minutes before)
     time_due = models.DateTimeField()
     is_active = models.BooleanField()
+
+    # Pin to the top if true
+    is_pinned = models.BooleanField(default=False)
+
+
 
     def __str__(self):
         return f"Task: {self.task}\nDue: {self.time_due}"
