7dab9e26a9d460f8589d7be0c24ee05f6e602ced
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-11-12 10:54:33 +0100

    Todo model created

--- Todo/models.py
+++ Todo/models.py
@@ -1,5 +1,13 @@
 from django.db import models
-
+from User.models import CustomUser
 # Create your models here.
 
 
+class Todo(models.Model):
+    task = models.TextField()
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    description = models.TextField()
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_due = models.DateTimeField()
+    is_active = models.BooleanField()
+    
