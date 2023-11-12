7b373a499798fc4a7751deb6b5e451bff348e0c1
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-11-12 11:40:32 +0100

    views for the Todo app

--- Todo/views.py
+++ Todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
+def index(request):
+    results = Todo.objects.filter(user=request.user)
+    print("results",results)
+    return render(request,"home.html",{"todos":results})
